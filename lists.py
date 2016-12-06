a1f0c078c55a9917681a7b864ab6ab7f3077e28b
Sachin Pathare <spathare@redhat.com>
2016-12-06 10:33:25 +0530

    operations on list example

---
+++ lists.py
@@ -0,0 +1,9 @@
+list = ['abcd',2121,2.34,'ross',21.22]
+tinylist = [123,'sac']
+
+print (list)        # Prints complete list
+print (list[0])     # print first element of list
+print (list[2:4])   # Prints element starting from 3rd to 5th element.
+print (list[2:])    # Prints element start from 3rd element 
+print (tinylist * 2)# Prints list tow times
+print (list + tinylist) # Concatenate lists
