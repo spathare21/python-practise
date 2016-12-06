d6bb1440cc0ffa128a67651b53cda3293178118f
Sachin Pathare <spathare@redhat.com>
2016-12-06 15:03:59 +0530

    loop and types

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "this is tow"
+
+tinydict = {'name':'sac','code':1234,'dept':'QA'}
+
+print (dict['one'])     # Prints value for one key
+print (dict[2])         # rptins value for 2 key.
+print (tinydict)        # Prints complete dictioanary
+print(tinydict.keys())  # Print all the keys
+print(tinydict.values())# pritns all the values
---
+++ rawstr.py
@@ -0,0 +1,2 @@
+print ('c:\\nowhere')
+print (r'c:\\nowhere')
