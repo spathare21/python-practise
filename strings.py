6757696939a0d89822ecc9c1d4c01ab8b1d4430b
Sachin Pathare <spathare@redhat.com>
2016-12-06 10:33:58 +0530

    strings operation example

---
+++ strings.py
@@ -0,0 +1,8 @@
+str = "Hello world!"
+
+print (str)      # print complete string
+print (str[0])   # print first character of string 
+print (str[2:5]) # print char starting from 3rd to 5th
+print (str[2:])  # print string starting from 3rd char
+print (str * 2)  # print string two times
+print (str + "Test") # print concatenated string
