0ba056e7722daf45393bad8e5e1764f37c91ce49
Sachin Pathare <spathare@redhat.com>
2016-12-07 14:29:47 +0530

    reg expression

---
+++ regexp.py
@@ -0,0 +1,14 @@
+#Regular expression examples
+import re
+line = "Cats are smarter than dogs"
+
+matchobj = re.match(r'(.*) are (.*?) .*',line,re.M|re.I)
+
+if matchobj :
+ print (matchobj.group())
+ print (matchobj.group(1))
+ print (matchobj.group(2))
+
+else :
+ print ("No match!!")
+
